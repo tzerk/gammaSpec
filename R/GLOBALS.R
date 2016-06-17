57dfa0c033e7bb5f9fa12c96bdc4c1013d8d5e84
tzerk <XnIASwkTCBlGgudcCsml>
2016-06-17 17:41:38 +0200

    first read_* and plot_* prototypes

---
+++ R/GLOBALS.R
@@ -0,0 +1,7 @@
+# A Vector of Keywords used as section headers in the SPE files and by which
+# the text can be split into separate parts.
+get_Keywords <- function() {
+  c("SPEC_ID", "SPEC_REM", "DATE_MEA", "MEAS_TIM", "DATA", "ROI", 
+    "PRESETS", "ENER_FIT", "MCA_CAL", "SHAPE_CAL")
+}
+
---
+++ R/plot_Spectrum.R
@@ -0,0 +1,54 @@
+plot_Spectrum <- function(data,
+                          type = c("bar", "line", "point"),
+                          xval = c("energy", "channel"), 
+                          yval = c("rate", "count"),
+                          info = TRUE,
+                          fill = TRUE,
+                          ...) {
+  
+  ## plot parameters
+  settings <- list(x = switch(xval[1],
+                              "energy" = data$DATA$energy,
+                              "channel" = data$DATA$channel),
+                   y = switch(yval[1],
+                              "count" = data$DATA$counts,
+                              "rate" = data$DATA$counts_norm),
+                   ylab = switch(yval[1],
+                                 "count" = "Count (a.u.)",
+                                 "rate" = "Count rate (1/s)"),
+                   xlab = switch(xval[1],
+                                 "energy" = "Energy (eV)",
+                                 "channel" = "Channel (a.u.)"),
+                   type = switch(type[1],
+                                  "bar" = "s",
+                                  "line" = "l",
+                                  "point" = "p"),
+                   main = "Gamma Energy Spectrum",
+                   col = "grey50",
+                   cex = 0.9,
+                   bty = "o",
+                   pch = 16)
+  
+  settings <- modifyList(settings, list(...))
+  
+  ## PLOT ----
+  par(cex = settings$cex)
+  do.call(plot, settings)
+  
+  # add legend
+  if (info) {
+    info <- data
+    info$DATA <- NULL
+    
+    labels <- lapply(info, function(x) paste(x, collapse = ", "))
+    labels <- paste0(paste0(names(labels), ": \n ", unlist(labels)), collapse = "\n")
+    
+    text(x = mean(settings$x) * 0.33, y = max(settings$y) * 0.75, labels = labels, cex = 0.7 * settings$cex, adj = 0)
+  }
+  
+  # add polygon fill
+  if (fill)
+    polygon(settings$x, pmin(settings$y), col = settings$col, border = NA)
+  
+  
+}
