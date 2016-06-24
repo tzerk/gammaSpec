ece1a6f2f8650a057b7147cdc7057bb42f5e0354
tzerk <XnIASwkTCBlGgudcCsml>
2016-06-24 16:14:48 +0200

    Total photon absorption data for NaI and SiO2

--- R/gammaSpec-package.R
+++ R/gammaSpec-package.R
@@ -1,4 +1,39 @@
-#' @importFrom graphics abline arrows lines mtext par
+#' A Collection of Functions to Analyse Gamma Spectrums
+#'
+#' <placeholder>
+#' 
+#' @name gammaSpec-package
+#' @aliases gammaSpec-package
+#' @docType package
+#' @author Christoph Burow (University of Cologne, Germany)
+#' @keywords package
+#' 
+#' @importFrom graphics abline arrows lines mtext par legend plot points polygon text
 #' @importFrom stats ecdf lm predict setNames
 #' @importFrom utils modifyList
+NULL
+
+
+#' Photon Cross Section Data
+#' 
+#' Total attenuation (with coherent scatter) of photons for NaI and SiO2, given
+#' in cm^2/g. Data taken from the NIST XCOM Photon Cross Section Database.
+#' 
+#' @name MassAttenuationCoefficients
+#' @docType data
+#' @format A \code{\link{list}} with two elements (\code{\link{data.frame}s})
+#' with two columns (\code{energy_MeV, attenuation_cm2g}: \cr
+#' \tabular{ll}{
+#' $NaI \tab Total attenuation for sodium iodide \cr
+#' $SiO2 \tab Total attenuation for silicon dioxide \cr
+#' }
+#' 
+#' @source 
+#' http://physics.nist.gov/PhysRefData/Xcom/html/xcom1-t.html
+#' 
+#' @keywords datasets
+#' 
+#' @examples 
+#' data(MassAttenuationCoefficients, envir = environment())
+#' 
 NULL
