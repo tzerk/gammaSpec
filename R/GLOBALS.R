7083c46f3f77af327b71bdde57a50fc780426abc
tzerk <XnIASwkTCBlGgudcCsml>
2017-05-22 16:38:43 +0200

    rename variable

--- R/GLOBALS.R
+++ R/GLOBALS.R
@@ -53,12 +53,12 @@ calc_Error <- function(N, dN, N1, dN1, N2, dN2, D1, dD1, D2, dD2, m) {
     (dD1 / (D1 - D2))^2 + (dD2 / (D1 - D2))^2 + (dN1 / (N1 - N2))^2 + (dN2 / (N1 - N2))^2
   )
   # intercept error
-  dN0 <- sqrt(
+  dD0 <- sqrt(
     dD1^2 + (-N1 * dm)^2 + (-m * dN1)^2
   )
   # dose rate error
   dD <- sqrt(
-    (N * dm)^2 + (m * dN)^2 + dN0^2
+    (N * dm)^2 + (m * dN)^2 + dD0^2
   )
   return(dD)
 }
